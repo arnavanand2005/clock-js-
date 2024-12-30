1ccdfc170d94677c81b9d01a429221fa08f116ac
arnavanand2005 <aa0978@srmist.edu.in>
2024-12-30 23:49:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function timeupdate(){
+    const now=new Date();
+    let hours=now.getHours();
+    const meridian = hours>=12 ? "PM" : "AM";
+    hours=hours%12||12;
+    hours=hours.toString().padStart(2,'0');
+    const minutes=now.getMinutes().toString().padStart(2,'0');
+    const seconds=now.getSeconds().toString().padStart(2,'0');
+    const timestring=`${hours}:${minutes}:${seconds}    ${meridian}`;
+    document.getElementById("time").textContent=timestring;
+}
+timeupdate();
+setInterval(timeupdate,1000); 
